2dbdeafd1c0ab4c129bbcff7398bdcc4e601be4e
davyleroy <d.nkurunziz@alustudent.com>
2023-05-17 10:34:13 -0700

    PROGRAMING_IS_COOL

---
+++ regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+# 0-simply_match_school.rb
+
+# Accept one argument from the command line
+input = ARGV[0]
+
+# Define the regular expression pattern
+pattern = /School/
+
+# Match the input against the regular expression
+if input =~ pattern
+  puts "#{input}$"
+else
+  puts ""
+end
+
