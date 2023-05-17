ba3cc88c14bfe28599a0eea1ca1bd44c974cb23f
davyleroy <d.nkurunziz@alustudent.com>
2023-05-17 10:30:55 -0700

    PROGRAMING_IS_COOL

---
+++ regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+# 1-repetition_token_0.rb
+
+# Accept the argument from the command line
+input_string = ARGV[0]
+
+# Define the regular expression pattern
+regex_pattern = /^(01)+0$/
+
+# Check if the input string matches the pattern
+if input_string.match?(regex_pattern)
+  puts "Input string matches the pattern"
+else
+  puts "Input string does not match the pattern"
+end
