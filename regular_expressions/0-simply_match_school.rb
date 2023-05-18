73771ef2078155d2058a7ae35b052fb78d39e09a
davyleroy <d.nkurunziz@alustudent.com>
2023-05-18 04:19:06 -0700

    PROGRAMING_IS_COOL

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,17 +1,2 @@
 #!/usr/bin/env ruby
-# 0-simply_match_school.rb
-
-# Accept one argument from the command line
-input = ARGV[0]
-
-# Define the regular expression pattern
-pattern = /\bSchool\b/
-
-# Match the input against the regular expression
-matches = input.scan(pattern)
-if input =~ pattern
-  puts "#{input}"
-else
-  puts ""
-end
-
+puts ARGV[0].scan(/School/).join
