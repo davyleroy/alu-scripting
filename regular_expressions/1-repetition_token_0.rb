d105fb5fa6c7423e838ef9202b022f46ffdc5cb6
davyleroy <d.nkurunziz@alustudent.com>
2023-05-18 04:13:33 -0700

    PROGRAMING_IS_COOL

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,15 +1,2 @@
 #!/usr/bin/env ruby
-# 1-repetition_token_0.rb
-
-# Accept the argument from the command line
-input_string = ARGV[0]
-
-# Define the regular expression pattern
-regex_pattern = /^(01)+0$/
-
-# Check if the input string matches the pattern
-if input_string.match?(regex_pattern)
-  puts "Input string matches the pattern"
-else
-  puts "Input string does not match the pattern"
-end
+puts ARGV[0].scan(/hbt{2,5}n/).join
